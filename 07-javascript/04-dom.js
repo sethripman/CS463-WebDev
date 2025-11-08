dc35a2c9bd9db59ef9384ebc10830db5ecbdc17c
sethripman <sethripman@gmail.com>
2025-11-08 00:02:13 -0800

    07

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -1,17 +1,20 @@
 // Exercise #1:
-// when the user clicks the 'copy' button, copy the user input to the output area
-
-// fetch JavaScript objects representing specific elements in the DOM
-
-// add an event listener on the target element
-
-// callback function to handle event
+let userInput = document.querySelector('#userInput1');
+let copy = document.querySelector('#copy');
+let output = document.querySelector('#output');
+copy.addEventListener('click', handleClick);
+function handleClick(event) {
+    console.log('click event', event);
+    output.textContent = userInput.value;
+}
 
 // Exercise #2:
-// when the user enters input text, copy the user input to the output area
-
-// fetch JavaScript objects representing specific elements in the DOM
-
-// add an event listener on the target element
-
-// callback function to handle event
+let userInput2 = document.querySelector('#userInput2');
+userInput2.addEventListener('input', handleInput);
+let element = document.createElement('div');
+element.setAttribute('class', 'output');
+document.querySelector('#inputEventExample').append(element);
+function handleInput(event) {
+    console.log('click event', event);
+    element.textContent = userInput2.value;
+}
--- 07-javascript/07-functional.js
+++ 07-javascript/07-functional.js
@@ -1,41 +1,43 @@
 const characters = [
-  { id: 1, name: 'Arya', house: 'Stark' },
-  { id: 4, name: 'Sansa', house: 'Stark' },
-  { id: 16, name: 'Eddard', house: 'Stark' },
-  { id: 17, name: 'Catelyn', house: 'Stark' },
-  { id: 20, name: 'Robb', house: 'Stark' },
-  { id: 28, name: 'Rickon', house: 'Stark' },
-  { id: 40, name: 'Jaime', house: 'Lannister' },
-  { id: 41, name: 'Cersei', house: 'Lannister' },
-  { id: 42, name: 'Tyrion', house: 'Lannister' },
-  { id: 50, name: 'Tywin', house: 'Lannister' },
-  { id: 53, name: 'Kevan', house: 'Lannister' },
-  { id: 75, name: 'Daenerys', house: 'Targaryen' },
-  { id: 77, name: 'Viserys', house: 'Targaryen' },
-  { id: 81, name: 'Rhaegar', house: 'Targaryen' },
-  { id: 83, name: 'Theon', house: 'Greyjoy' },
-  { id: 84, name: 'Yara', house: 'Greyjoy' },
-  { id: 87, name: 'Davos', house: 'Seaworth' },
-  { id: 88, name: 'Brienne', house: 'Tarth' },
+    { id: 1, name: 'Arya', house: 'Stark' },
+    { id: 4, name: 'Sansa', house: 'Stark' },
+    { id: 16, name: 'Eddard', house: 'Stark' },
+    { id: 17, name: 'Catelyn', house: 'Stark' },
+    { id: 20, name: 'Robb', house: 'Stark' },
+    { id: 28, name: 'Rickon', house: 'Stark' },
+    { id: 40, name: 'Jaime', house: 'Lannister' },
+    { id: 41, name: 'Cersei', house: 'Lannister' },
+    { id: 42, name: 'Tyrion', house: 'Lannister' },
+    { id: 50, name: 'Tywin', house: 'Lannister' },
+    { id: 53, name: 'Kevan', house: 'Lannister' },
+    { id: 75, name: 'Daenerys', house: 'Targaryen' },
+    { id: 77, name: 'Viserys', house: 'Targaryen' },
+    { id: 81, name: 'Rhaegar', house: 'Targaryen' },
+    { id: 83, name: 'Theon', house: 'Greyjoy' },
+    { id: 84, name: 'Yara', house: 'Greyjoy' },
+    { id: 87, name: 'Davos', house: 'Seaworth' },
+    { id: 88, name: 'Brienne', house: 'Tarth' },
 ];
 
 const mapNameFamily = (characters) => {
-  // Return an array with the name and house of each character
-  // Format: "Arya, of House Stark"
-
-  return characters;
+    const intros = characters.map(
+        (item) => `${item.name}, of House ${item.house}`
+    );
+    return intros;
 };
 
 const filterFamily = (characters, house) => {
-  // Return an array with only the characters from a given house
-
-  return characters;
+    const starks = characters.filter((item) => item.house === house);
+    return starks;
 };
 
 const reduceHouses = (characters) => {
-  // Return an object with the number of characters from each house
+    const houses = characters.reduce((total, item) => {
+        total.hasOwnProperty(item.house) ? total[item.house]++ : (total[item.house] = 1);
+        return total;
+    }, {});
 
-  return characters;
+    return houses;
 };
 
 console.log(mapNameFamily(characters));
